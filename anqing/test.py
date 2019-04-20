5b3ca0715a1ae08d4b2c910ba58a27302f3b6ef8
gyh62357 <623575595@qq.com>
2019-04-20 22:21:48 +0800

    测试1

--- anqing/test.py
+++ anqing/test.py
@@ -16,6 +16,7 @@ for i in range(1,3):
     print('强力','棉结','条干','单强','毛羽')
     print(a[i][40],a[i][41],a[i][42],a[i][43],a[i][44])
 #数据存到Excel中
+#数据存到Excel中
 # df = pd.DataFrame(connectionsql.resList)
 # df.to_excel("C://Users//Administrator//Desktop//test//data.xlsx")
 
